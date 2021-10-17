6cb16a35d5348b137559749605c7068e7eac9530
alikhamesy <alikhamesy0@gmail.com>
2021-10-16 18:04:00 -0600

    init 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import styles from './App.module.scss'
+
+import ThemeProvider from './context/Theme'
+
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <ThemeProvider>
+      <div className={styles.App}>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
   )
 }
 
