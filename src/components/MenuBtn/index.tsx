5a6d4a8bb4233a5f11d8f8420cd710262ce22770
Joshua Peng <jpa95@sfu.ca>
2021-11-01 22:55:44 -0700

    menu cube btn with text

---
+++ src/components/MenuBtn/index.tsx
@@ -0,0 +1,38 @@
+import styles from './MenuBtn.module.scss'
+import { CSSProperties, ReactNode } from 'react'
+import { IImage } from '../Image'
+import Image from '../Image'
+import sponsors from '../../assets/pageIcons/sponsors.png'
+import { ReactComponent as Logo } from '../../assets/pageIcons/smallCube.svg'
+
+interface IMenuBtn extends IImage {
+  fill?: string
+  stroke?: string
+  link?: string
+  style?: CSSProperties
+  children?: ReactNode
+}
+
+const MenuBtn = ({
+  className,
+  src,
+  alt,
+  link,
+  fill,
+  stroke,
+  ...props
+}: IMenuBtn) => {
+  return (
+    <button className={styles.container}>
+      <Logo fill={fill} stroke={stroke} />
+      <div className={styles.imageLink}>
+        <Image className={styles.img} src={src} alt={alt} />
+        <p className={styles.btnText} style={props.style}>
+          {link}
+        </p>
+      </div>
+    </button>
+  )
+}
+
+export default MenuBtn
